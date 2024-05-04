1c5b56d7a435d06f1de06cfc55abdb1cc658545e
paolamip <pmeave13@gmail.com>
2024-05-03 18:50:14 -0700

    se ha agregado otra funcion

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// Cálculo del IMC
 const btnCalcular = document.getElementById('btnCalcular');
 const btnLimpiar = document.getElementById('btnLimpiar');
 
@@ -8,8 +9,44 @@ btnLimpiar.addEventListener('click', function() {
 });
 
 btnCalcular.addEventListener('click', function() {
-    let peso = document.getElementById('peso').value;
-    let altura = document.getElementById('altura').value;
+    let peso = parseFloat(document.getElementById('peso').value);
+    let altura = parseFloat(document.getElementById('altura').value);
     let imc = peso / (altura * altura);
     document.getElementById('resultado').value = imc.toFixed(1);
+});
+
+// Practica2
+const btnCalcularp2 = document.getElementById('btnCalcularp2');
+const btnLimpiarp2 = document.getElementById('btnLimpiarp2');
+
+btnLimpiarp2.addEventListener('click', function() {
+    document.getElementById('idNumero1').value = '';
+    document.getElementById('idNumero2').value = '';
+    document.getElementById('idResultado').value = '';
+});
+
+btnCalcularp2.addEventListener('click', function() {
+    const option = document.getElementById('idOpcion').value;
+    const idNumero1 = parseFloat(document.getElementById('idNumero1').value);
+    const idNumero2 = parseFloat(document.getElementById('idNumero2').value);
+    const txtres = document.getElementById('idResultado');
+
+    let res = 0;
+
+    switch (parseInt(option)) {
+        case 1:
+            res = idNumero1 + idNumero2;
+            break;
+        case 2:
+            res = idNumero1 - idNumero2;
+            break;
+        case 3:
+            res = idNumero1 * idNumero2;
+            break;
+        case 4:
+            res = idNumero1 / idNumero2;
+            break;
+    }
+
+    txtres.value = res;
 });
